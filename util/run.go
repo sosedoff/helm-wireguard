68297292f1a51165fa705c4e35ed21d2c5b3a5ca
Dan Sosedoff <dan.sosedoff@gmail.com>
2022-12-28 13:01:54 -0600

    Remove unused code

--- util/run.go
+++ util/run.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"log"
 	"os"
 	"os/exec"
 )
@@ -12,29 +10,4 @@ func runCmd(name string, args ...string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
-}
-
-func runWithOutput(name string, args ...string) (string, error) {
-	output := bytes.NewBuffer(nil)
-
-	cmd := exec.Command(name, args...)
-	cmd.Stdout = output
-	cmd.Stderr = os.Stderr
-
-	return output.String(), cmd.Run()
-}
-
-func checkInterface(name string) error {
-	output := bytes.NewBuffer(nil)
-
-	cmd := exec.Command("wg", "show", name)
-	cmd.Stdout = output
-	cmd.Stderr = output
-
-	err := cmd.Run()
-	if err != nil {
-		log.Println("wireguard interface check failed. output:", output.String())
-	}
-
-	return err
 }
